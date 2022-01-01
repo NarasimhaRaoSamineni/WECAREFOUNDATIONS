bddc4cf2a9ec58066708bf846b43e77bbda36f0e
MandepuRajkumar <66173215+MandepuRajkumar@users.noreply.github.com>
2022-01-02 03:33:10 +0530

    Add files via upload

---
+++ src/main/java/com/smart/smartcontactmanager/entities/Contactus.java
@@ -0,0 +1,95 @@
+package com.smart.smartcontactmanager.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="CONTACTUS")
+public class Contactus {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int cuId;
+    private String name;
+    private String secondName;
+    private String work;
+    private String email;
+    private String phone;
+    @Column(length=5000)
+    private String description;
+
+    @Override
+    public String toString() {
+        return "Contactus{" +
+                "cuId=" + cuId +
+                ", name='" + name + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", work='" + work + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    public Contactus() {
+        super();
+    }
+
+    public int getcuId() {
+        return cuId;
+    }
+
+    public void setcuId(int cuId) {
+        this.cuId = cuId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getWork() {
+        return work;
+    }
+
+    public void setWork(String work) {
+        this.work = work;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
+}
